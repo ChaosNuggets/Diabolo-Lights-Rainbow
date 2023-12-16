82539fd1c762a6b33c364fbcaa06fd83347e8153
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2023-12-16 03:22:51 -0500

    added a lot of comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,25 @@
 using namespace Diabolo_Light;
 
 Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_RGB + NEO_KHZ800);
-
 uint16_t hue = 0;
 
 void setup() {
     pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+
+    // Initialize the diabolo light with 1 mode (not including the off mode)
+    // and make the board set the hue to 0 whenever the board turns on
     begin(1, [](){ hue = 0; });
 }
 
 void loop() {
+    // Read button input and determine whether the mode should be changed.
+    // Also determine whether or not the board should shut off.
     handle_button();
 
+    // Do the rainbow effect
     for (int i = 0; i < NUM_LEDS; i++) {
         pixels.setPixelColor(i, pixels.ColorHSV(hue, 255, 255));
     }
     pixels.show();
-
     hue += 5;
 }
